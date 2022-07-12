b4fc273ffc9be7db94334f2f4a3afbd3dc89415c
hp <hp@LAPTOP-J6TM91QC>
2022-07-12 12:39:51 +0530

    files added

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Speech_recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(1, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(303, 548);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(310, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(517, 353);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(827, 550);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.IO;
+using System.Management;
+
+namespace Speech_recognition
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Sara = new SpeechSynthesizer();   
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();  
+        Random random = new Random();
+        int recTimeOut = 0;    //counter between 1 and 2 
+        DateTime timenow = DateTime.Now;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recognizer.SetInputToDefaultAudioDevice();        // default audio to device such as microphone or speaker
+            recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(recognizer_SpeechRecognized);
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Startlistening_SpeechRecognized);
+
+        }
+
+        private void recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            recTimeOut = 0;
+        }
+
+        private void Startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+
+            if(speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Sara.SpeakAsync("Yes, I am here");
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if(speech == "Hello")
+            {
+                Sara.SpeakAsync("Hello Shreya");
+            }
+            if (speech == "How are you")
+            {
+                Sara.SpeakAsync("I am working good");
+            }
+            if (speech == "What time is it")
+            {
+                Sara.SpeakAsync(DateTime.Now.ToString("h mm tt"));   // for example 1.45 PM
+            }
+            if(speech == "Stop listening")
+            {
+                Sara.SpeakAsyncCancelAll();
+                ranNum = random.Next(1,2);
+                if(ranNum == 1)
+                {
+                    Sara.SpeakAsync("Yes sir");
+                }
+                          
+                if(ranNum == 2)
+                {
+                    Sara.SpeakAsync("I am sorry I will be quiet");
+                }                           
+            }
+            if(speech == "Stop talking")
+            {
+                Sara.SpeakAsync("If you need me just ask");
+                recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            if(speech == "Show commands")
+            {
+                string[] commands = (File.ReadAllLines(@"DefaultCommands.txt"));
+                listBox1.Items.Clear();
+                listBox1.SelectionMode = SelectionMode.None;
+                listBox1.Visible = true;
+                foreach(string command in commands)
+                {
+                    listBox1.Items.Add(command);
+                }
+            }
+            if(speech == "Hide Commands")
+            {
+                listBox1.Visible = false;
+            }
+            if(speech == "Close it")
+            {
+                Application.Exit();
+
+            }
+            if(speech == "What is the battery status")
+            {
+                BatteryStatus();
+            }
+            if(speech == "Open google")
+            {
+                System.Diagnostics.Process.Start("https://www.google.com/");
+            }
+            if(speech == "Play DNA")
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=MBdVXkSdhwU");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(recTimeOut == 10)
+            {
+                recognizer.RecognizeAsyncCancel();
+            }
+            else if(recTimeOut == 11)
+            {
+                timer1.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                recTimeOut = 0;
+            }
+        }
+        private void BatteryStatus()
+        {
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_Battery");
+            var allBatteries = wmi.GetInstances();
+
+            foreach(var battery in allBatteries)
+            {
+                int estimatedChargeRemaining = Convert.ToInt32(battery["EstimatedChargeremaining"]);
+                Sara.SpeakAsync("The estimated charge is" + estimatedChargeRemaining + "%");
+                
+            }
+        }
+        
+    }
+}
